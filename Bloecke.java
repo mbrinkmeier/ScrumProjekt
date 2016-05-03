7d780e226a997775948df95d239dfab4b36dd8b9
pkechlo <pkechlo@uos.de>
2016-05-03 09:58:16 +0200

    Hindernisse als zerstoerbare und unzerstoerbare Objekte hinzugefuegt;Bilder hinzugefuegt fuer die jeweiligen Objekte;

---
+++ Bloecke.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Bloecke here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Bloecke extends Actor
+{
+    /**
+     * Act - do whatever the Bloecke wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        // Add your action code here.
+    }    
+}
