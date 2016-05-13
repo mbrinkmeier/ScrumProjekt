4473671dd688ca25e7fc0ca1045d4c88d120ae0c
Laura <lhembrock@uos.de>
2016-05-13 13:04:58 +0200

    Holz auf 10x10

--- Holz.java
+++ Holz.java
@@ -8,6 +8,9 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Holz extends ZerstoerbareBloecke
 {
+    public Holz() {
+        this.getImage().scale(10, 10);
+    }
     /**
      * Act - do whatever the Holz wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
