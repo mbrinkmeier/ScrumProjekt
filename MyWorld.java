25f7d5d7b4c5fc4b36328a73744e0125d8a3b435
Mulli <Mulli>
2016-05-17 09:56:20 +0200

    Umrandung gesetzt und Spielfigur verschoben

--- MyWorld.java
+++ MyWorld.java
@@ -23,10 +23,33 @@ public class MyWorld extends World
         // Create a new world with 50x50 cells with a cell size of 10x10 pixels.
         super(50, 50, 10); 
         
+        Stein[] steine = new Stein[36];
         
-                
+        // erste Reihe setzen
+        for(int i=0; i<10; i++){
+            steine[i] = new Stein();
+            this.addObject(steine[i], i*5+2, 2);
+        }
+        
+        //letzte Reihe setzen
+        for(int i=10; i<20; i++){
+            steine[i] = new Stein();
+            this.addObject(steine[i], (i-10)*5+2, 47);
+        }
+        
+        //linke Reihe
+        for(int i=20; i<28; i++){
+            steine[i] = new Stein();
+            this.addObject(steine[i], 2, (i-20)*5+7);
+        }
+        
+        //rechte Reihe
+        for(int i=28; i<36; i++){
+            steine[i] = new Stein();
+            this.addObject(steine[i], 47, (i-28)*5+7);
+        }
+              
         Spielfigur spielfigur = new Spielfigur();
-        
-        addObject(spielfigur, 1, 1);
+        addObject(spielfigur, 25, 25);
     }
 }
